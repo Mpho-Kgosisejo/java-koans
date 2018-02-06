59b78a50cf061c5c9e2cc9912b97ad1d10f7b82c
Mpho Kgosisejo <mphok@bbd.co.za>
2018-02-06 16:55:14 +0200

    first push

---
+++ koans/src/java7/AboutDiamondOperator.java
@@ -0,0 +1,49 @@
+package java7;
+
+import com.sandwich.koan.Koan;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import static com.sandwich.koan.constant.KoanConstants.__;
+import static com.sandwich.util.Assert.assertEquals;
+
+public class AboutDiamondOperator {
+
+    @Koan
+    public void diamondOperator() {
+        String[] animals = {"Dog", "Cat", "Tiger", "Elephant", "Zebra"};
+        //Generic type of array list inferred - empty <> operator
+        List<String> animalsList = new ArrayList<>(Arrays.asList(animals));
+		ArrayList<String> actual = new ArrayList<String>();
+        actual.add("Dog");
+        actual.add("Cat");
+        actual.add("Tiger");
+        actual.add("Elephant");
+        actual.add("Zebra");
+		
+		assertEquals(animalsList, actual);
+    }
+
+    @Koan
+    public void diamondOperatorInMethodCall() {
+        String[] animals = {"Dog", "Cat", "Tiger", "Elephant", "Zebra"};
+        //type of new ArrayList<>() inferred from method parameter
+        List<String> animalsList = fill(new ArrayList<>());
+		ArrayList<String> actual = new ArrayList<String>();
+        actual.add("Dog");
+        actual.add("Cat");
+        actual.add("Tiger");
+        actual.add("Elephant");
+        actual.add("Zebra");
+        assertEquals(animalsList, actual);
+    }
+
+    private List<String> fill(List<String> list) {
+        String[] animals = {"Dog", "Cat", "Tiger", "Elephant", "Zebra"};
+        list.addAll(Arrays.asList(animals));
+        return list;
+    }
+
+}
